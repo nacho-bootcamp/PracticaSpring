180251da1d8f070a6189d9d77c6ae555543b73a6
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-11-11 23:48:54 -0300

    crear una clase de tipo dominio

--- Ejercicio2/HolaMundoConThymeleaf/src/main/java/com/example/controllers/ControladorRest.java
+++ Ejercicio2/HolaMundoConThymeleaf/src/main/java/com/example/controllers/ControladorRest.java
@@ -1,6 +1,11 @@
 package com.example.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.domain.Individuo;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,17 +13,25 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 @CrossOrigin(origins = "*")
-
+@Slf4j
 public class ControladorRest {
   @Value("${indice.hola}")
   private String hola;
 
   @GetMapping("")
   public String comienzo(Model model) {
-    String saludo = "hola ya funciona no se como se arreglo";
+
+    Individuo individuo = new Individuo();
+    individuo.setNombre("Nacho");
+    individuo.setApellido("Cardozo");
+    individuo.setCorreo("ignacio@gmail.com");
+    individuo.setTelefono("491865");
+    individuo.setEdad("22");
+
+    log.info("Estoy ejecutando el controlador MVC");
     model.addAttribute("titulo", "titulo ya funciona asi");
     model.addAttribute("cuerpo", "cuerpo ya funciona asi");
-    model.addAttribute("saludo", saludo);
+    model.addAttribute("individuo", individuo);
     model.addAttribute("hola", hola);
 
     return "indice";
