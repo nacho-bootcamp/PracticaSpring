f65ea50167dc991cb9c351531bc3b031b6ef58c0
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-11-12 12:01:26 -0300

    Crear listas

--- Ejercicio2/HolaMundoConThymeleaf/src/main/java/com/example/controllers/ControladorRest.java
+++ Ejercicio2/HolaMundoConThymeleaf/src/main/java/com/example/controllers/ControladorRest.java
@@ -6,6 +6,9 @@ import com.example.domain.Individuo;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,11 +31,25 @@ public class ControladorRest {
     individuo.setTelefono("491865");
     individuo.setEdad("22");
 
+    Individuo individuo2 = new Individuo();
+    individuo2.setNombre("Eri");
+    individuo2.setApellido("Condori");
+    individuo2.setCorreo("eri@gmail.com");
+    individuo2.setTelefono("4791865");
+    individuo2.setEdad("25");
+
+    // List<Individuo> listaDeIndividuos = new ArrayList<>();
+    // listaDeIndividuos.add(individuo);
+    // listaDeIndividuos.add(individuo);
+
+    List individuos = Arrays.asList(individuo, individuo2);
+
     log.info("Estoy ejecutando el controlador MVC");
     model.addAttribute("titulo", "titulo ya funciona asi");
     model.addAttribute("cuerpo", "cuerpo ya funciona asi");
     model.addAttribute("individuo", individuo);
     model.addAttribute("hola", hola);
+    model.addAttribute("individuos", individuos);
 
     return "indice";
   }
