2d2e289bafe8c132850ac8750e1f87c15af60bf4
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-11-12 22:02:06 -0300

    Configuracion y conectar base de datos part2

--- Ejercicio3/HolaMundoUsandoSpringData/src/main/java/com/example/domain/Individuo.java
+++ Ejercicio3/HolaMundoUsandoSpringData/src/main/java/com/example/domain/Individuo.java
@@ -1,9 +1,19 @@
 package com.example.domain;
 
+import java.io.Serializable;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data // nos ahorra al hacer el get y set de estos atributos
-public class Individuo {
+@Entity
+@Table(name = "individuo")
+public class Individuo implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id_individuo;
   private String nombre;
   private String apellido;
   private String telefono;
